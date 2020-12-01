b3dda081756ccf9ff1129fc8806c49b1bc8ad19d
Bryan Burgers <bryan@burgers.io>
2020-12-01 09:26:09 -0600

    01b

--- day-01/src/main.rs
+++ day-01/src/main.rs
@@ -4,13 +4,24 @@ fn main() {
     let stdin = std::io::stdin();
     let lines = stdin.lock().lines();
 
-    let numbers: Vec<_> = lines.map(|line| line.unwrap().parse::<u64>().unwrap()).collect();
-    println!("{:?}", numbers);
+    let numbers: Vec<_> = lines
+        .map(|line| line.unwrap().parse::<u64>().unwrap())
+        .collect();
 
     for n in &numbers {
         for m in &numbers {
             if n + m == 2020 {
-                println!("n = {}, m = {}, n*m = {}", n, m, n*m);
+                println!("n = {}, m = {}, n*m = {}", n, m, n * m);
+            }
+        }
+    }
+
+    for l in &numbers {
+        for n in &numbers {
+            for m in &numbers {
+                if l + n + m == 2020 {
+                    println!("l = {}, n = {}, m = {}, l*n*m = {}", l, n, m, l * n * m);
+                }
             }
         }
     }
