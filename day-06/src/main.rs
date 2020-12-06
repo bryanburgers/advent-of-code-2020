dbd69af274de90d845f9bb404419443e21242d35
Bryan Burgers <bryan@burgers.io>
2020-12-06 08:29:21 -0600

    06a

---
+++ day-06/src/main.rs
@@ -0,0 +1,57 @@
+use std::collections::HashSet;
+use std::io::Read;
+
+fn main() {
+    let mut input = String::new();
+    std::io::stdin().read_to_string(&mut input).unwrap();
+
+    let groups = input
+        .split("\n\n")
+        .map(|block| block.parse())
+        .collect::<Result<Vec<Group>, _>>()
+        .unwrap();
+
+    let r: usize = groups.iter().map(|group| group.total_yeses()).sum();
+    println!("{}", r);
+}
+
+#[derive(Clone, Debug)]
+struct Group {
+    forms: Vec<Form>,
+}
+
+impl Group {
+    fn total_yeses(&self) -> usize {
+        let all_yeses = self.forms.iter().fold(HashSet::new(), |hashset, form| {
+            hashset.union(&form.answers).cloned().collect()
+        });
+        all_yeses.len()
+    }
+}
+
+impl std::str::FromStr for Group {
+    type Err = &'static str;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let forms = s
+            .trim()
+            .split('\n')
+            .map(|form| form.parse())
+            .collect::<Result<Vec<Form>, _>>()?;
+        Ok(Self { forms })
+    }
+}
+
+#[derive(Clone, Debug)]
+struct Form {
+    answers: HashSet<char>,
+}
+
+impl std::str::FromStr for Form {
+    type Err = &'static str;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let answers = s.trim().chars().collect();
+        Ok(Self { answers })
+    }
+}
