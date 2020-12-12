1d75b74fd6f62a4eedf086e5bb5e16d134300170
Bryan Burgers <bryan@burgers.io>
2020-12-12 13:26:00 -0600

    12b

--- day-12/src/main.rs
+++ day-12/src/main.rs
@@ -10,10 +10,17 @@ fn main() {
         .collect();
 
     let mut ferry_state = FerryState::default();
-    for action in actions {
-        ferry_state.apply(action);
+    for action in &actions {
+        ferry_state.apply(*action);
     }
     let manhatten_distance = ferry_state.x.abs() + ferry_state.y.abs();
+    println!("{}", manhatten_distance);
+
+    let mut waypoint_ferry_state = WaypointFerryState::default();
+    for action in &actions {
+        waypoint_ferry_state.apply(*action);
+    }
+    let manhatten_distance = waypoint_ferry_state.x.abs() + waypoint_ferry_state.y.abs();
     println!("{}", manhatten_distance);
 }
 
@@ -63,6 +70,97 @@ impl FerryState {
                 self.x += len;
             }
         }
+    }
+}
+
+#[derive(Clone, Copy, Debug)]
+struct WaypointFerryState {
+    x: i64,
+    y: i64,
+    waypoint_x: i64,
+    waypoint_y: i64,
+}
+
+impl Default for WaypointFerryState {
+    fn default() -> Self {
+        WaypointFerryState {
+            x: 0,
+            y: 0,
+            waypoint_x: 10,
+            waypoint_y: -1,
+        }
+    }
+}
+
+impl WaypointFerryState {
+    fn apply(&mut self, action: Action) {
+        match action {
+            Action::MoveDirection(direction, len) => self.move_direction(direction, len),
+            Action::Forward(len) => {
+                for _ in 0..len {
+                    self.x += self.waypoint_x;
+                    self.y += self.waypoint_y;
+                }
+            }
+            Action::RotateLeft(rotation) => match rotation {
+                Rotation::Ninety => {
+                    self.rotate_left();
+                }
+                Rotation::OneEighty => {
+                    self.rotate_left();
+                    self.rotate_left();
+                }
+                Rotation::TwoSeventy => {
+                    self.rotate_left();
+                    self.rotate_left();
+                    self.rotate_left();
+                }
+            },
+            Action::RotateRight(rotation) => match rotation {
+                Rotation::Ninety => {
+                    self.rotate_right();
+                }
+                Rotation::OneEighty => {
+                    self.rotate_right();
+                    self.rotate_right();
+                }
+                Rotation::TwoSeventy => {
+                    self.rotate_right();
+                    self.rotate_right();
+                    self.rotate_right();
+                }
+            },
+        }
+    }
+
+    fn move_direction(&mut self, direction: Direction, len: i64) {
+        match direction {
+            Direction::North => {
+                self.waypoint_y -= len;
+            }
+            Direction::South => {
+                self.waypoint_y += len;
+            }
+            Direction::West => {
+                self.waypoint_x -= len;
+            }
+            Direction::East => {
+                self.waypoint_x += len;
+            }
+        }
+    }
+
+    fn rotate_left(&mut self) {
+        let new_waypoint_y = self.waypoint_x * -1;
+        let new_waypoint_x = self.waypoint_y;
+        self.waypoint_x = new_waypoint_x;
+        self.waypoint_y = new_waypoint_y;
+    }
+
+    fn rotate_right(&mut self) {
+        self.rotate_left();
+        self.rotate_left();
+        self.rotate_left();
     }
 }
 
