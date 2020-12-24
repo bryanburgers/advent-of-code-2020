7cf595a880c2726e7a55cc2105e1c4d7fe8447ff
Bryan Burgers <bryan@burgers.io>
2020-12-24 14:35:21 -0600

    24b

--- day-24/src/main.rs
+++ day-24/src/main.rs
@@ -1,4 +1,4 @@
-use std::collections::HashMap;
+use std::collections::HashSet;
 use std::io::Read;
 
 fn main() {
@@ -9,6 +9,11 @@ fn main() {
     for line in input.trim().lines() {
         let tile = DirectionParser::new(line).fold(Tile::reference(), |tile, dir| tile.dir(dir));
         floor.flip(tile);
+    }
+    println!("{}", floor.count_blacks());
+
+    for _ in 0..100 {
+        floor = floor.generation();
     }
     println!("{}", floor.count_blacks());
 }
@@ -85,21 +90,50 @@ impl Tile {
             W => Tile::new(self.x - 2, self.y),
         }
     }
+
+    pub fn neighbors(&self) -> NeighborIterator {
+        NeighborIterator::new(*self)
+    }
+}
+
+#[derive(Debug, Clone, Copy)]
+pub struct NeighborIterator {
+    tile: Tile,
+    next: Option<Direction>,
+}
+
+impl NeighborIterator {
+    fn new(tile: Tile) -> Self {
+        NeighborIterator {
+            tile,
+            next: Some(Direction::NW),
+        }
+    }
+}
+
+impl Iterator for NeighborIterator {
+    type Item = Tile;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        use Direction::*;
+        let r = self.next;
+        self.next = match r {
+            Some(NW) => Some(NE),
+            Some(NE) => Some(E),
+            Some(E) => Some(SE),
+            Some(SE) => Some(SW),
+            Some(SW) => Some(W),
+            Some(W) => None,
+            None => None,
+        };
+        r.map(|r| self.tile.dir(r))
+    }
 }
 
 #[derive(Debug, Clone, Copy, Hash, Eq, PartialEq)]
 pub enum Color {
     White,
     Black,
-}
-
-impl Color {
-    fn flip(self) -> Self {
-        match self {
-            Color::White => Color::Black,
-            Color::Black => Color::White,
-        }
-    }
 }
 
 impl Default for Color {
@@ -110,20 +144,63 @@ impl Default for Color {
 
 #[derive(Debug, Default)]
 pub struct Floor {
-    tiles: HashMap<Tile, Color>,
+    tiles: HashSet<Tile>,
 }
 
 impl Floor {
     fn flip(&mut self, tile: Tile) {
-        let color = self.tiles.entry(tile).or_default();
-        *color = color.flip();
+        if !self.tiles.insert(tile) {
+            self.tiles.remove(&tile);
+        }
     }
 
     fn count_blacks(&self) -> usize {
-        self.tiles
-            .values()
-            .filter(|value| **value == Color::Black)
-            .count()
+        self.tiles.len()
+    }
+
+    fn get(&self, tile: Tile) -> Color {
+        if self.tiles.contains(&tile) {
+            Color::Black
+        } else {
+            Color::White
+        }
+    }
+
+    fn set(&mut self, tile: Tile, color: Color) {
+        if color == Color::Black {
+            self.tiles.insert(tile);
+        } else {
+            self.tiles.remove(&tile);
+        }
+    }
+
+    fn generation(&self) -> Self {
+        let mut next_day = Self::default();
+        let mut consider_tiles: HashSet<Tile> = HashSet::new();
+        for tile in &self.tiles {
+            for neighbor in tile.neighbors() {
+                consider_tiles.insert(neighbor);
+            }
+            consider_tiles.insert(*tile);
+        }
+
+        for tile in consider_tiles {
+            let mut nearby_blacks = 0;
+            for neighbor in tile.neighbors() {
+                if self.get(neighbor) == Color::Black {
+                    nearby_blacks += 1;
+                }
+            }
+            let color = match (self.get(tile), nearby_blacks) {
+                (Color::Black, 1) => Color::Black,
+                (Color::Black, 2) => Color::Black,
+                (Color::Black, _) => Color::White,
+                (Color::White, 2) => Color::Black,
+                (Color::White, _) => Color::White,
+            };
+            next_day.set(tile, color);
+        }
+        next_day
     }
 }
 
@@ -149,5 +226,21 @@ mod tests {
         assert_eq!(parser.next(), Some(SE));
         assert_eq!(parser.next(), Some(W));
         assert_eq!(parser.next(), None);
+    }
+
+    #[test]
+    fn neighbors() {
+        use Direction::*;
+
+        let tile = Tile::reference().dir(NW).dir(NW).dir(W).dir(NE);
+
+        let neighbors: Vec<Tile> = tile.neighbors().collect();
+        assert!(neighbors.contains(&tile.dir(NW)));
+        assert!(neighbors.contains(&tile.dir(NE)));
+        assert!(neighbors.contains(&tile.dir(E)));
+        assert!(neighbors.contains(&tile.dir(SE)));
+        assert!(neighbors.contains(&tile.dir(SW)));
+        assert!(neighbors.contains(&tile.dir(W)));
+        assert_eq!(neighbors.len(), 6);
     }
 }
